1764abbb7bba4f5122f7c314817171d30a2bca52
Honey Sharma <honeysharma0224@gmail.com>
2024-08-05 10:43:19 +0530

    Addedd file

---
+++ test/python_test.py
@@ -0,0 +1,14 @@
+import math
+import pytest
+
+
+def test_sqrt():
+    num =25
+    assert math.sqrt(num)==5
+
+def testsquare():
+    assert 6*6 == 40
+
+def testequality():
+    assert 10==11
+
---
+++ test/test_compare.py
@@ -0,0 +1,11 @@
+def test_greater():
+    num =100
+    assert num>50
+
+def test_smaller():
+    num = 50
+    assert num<100
+
+def test_greaterorsmaller():
+    num = 50
+    assert num>=70
