4bc548b1a540d1d91e22b4f4eb424a07f5d28250
Honey Sharma <113124234+honeyy02@users.noreply.github.com>
2024-08-06 10:51:46 +0530

    Update test_app.py to test coverage

--- test/test_app.py
+++ test/test_app.py
@@ -4,6 +4,6 @@ def test_add():
     assert add(6,4)==10
     assert add(7,2)==10
 
-def test_sub():
-    assert sub(4,2)==2
-    assert sub(8,5)==3    
+# def test_sub():
+#     assert sub(4,2)==2
+#     assert sub(8,5)==3    
