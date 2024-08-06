b3cebe95aeeca50480d53e9ca126a2fae459045e
Honey Sharma <113124234+honeyy02@users.noreply.github.com>
2024-08-06 10:55:22 +0530

    Update test_app.py
    Added success full test cases

--- test/test_app.py
+++ test/test_app.py
@@ -2,8 +2,8 @@ from my_app.app import add, sub
 
 def test_add():
     assert add(6,4)==10
-    assert add(7,2)==10
+    assert add(7,3)==10
 
-# def test_sub():
-#     assert sub(4,2)==2
-#     assert sub(8,5)==3    
+def test_sub():
+    assert sub(4,2)==2
+    assert sub(8,5)==3    
